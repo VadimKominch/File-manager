0a3638fb1ce2f2670ba8913113dd399b83397833
Vadim_kominch <vadzim.kominch@digitalmara.com>
2022-06-12 16:26:47 +0300

    feat: add os info representation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import process from 'process'
 
+import * as osOperations from './system_info/os_command.js'
+
+const osCommands = osOperations.osOperations
 
 const username = process.argv.slice(2)[0].split("=")[1]
+printCommandInfo(`Welcome to the File Manager, ${username}!\n`)
 
-process.stdout.write(`Welcome to the File Manager, ${username}!`)
+process.stdin.addListener("data",(data)=>{
+    handleInput(data.toString().replace("\r","").replace("\n",""))    
+})
+
+process.on('SIGINT', function() {
+    printCommandInfo(`Thank you for using File Manager,  ${username}!\n`)
+    process.exit(0)
+})
+
+function handleInput(data) {
+    const args = data.split(" ")
+    if(args[0] == "os") {
+        const result = osCommands[args[1].replace("--","")]
+        console.log(result())
+    }
+    if(data === ".exit") {
+        process.emit("SIGINT");
+    }
+}
 
 
-
-process.stdout.write(`Thank you for using File Manager,  ${username}!`)
+function printCommandInfo(info) {
+    process.stdout.write(info)
+}
