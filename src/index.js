6cdf1a4f75bb465340f02ea57f96b0823963f3b0
Vadim_kominch <vadzim.kominch@digitalmara.com>
2022-06-12 15:17:54 +0300

    feat: add args parse

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+import process from 'process'
+
+
+const username = process.argv.slice(2)[0].split("=")[1]
+
+process.stdout.write(`Welcome to the File Manager, ${username}!`)
+
+
+
+process.stdout.write(`Thank you for using File Manager,  ${username}!`)
